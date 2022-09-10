6e9aa5464f471f47db3938735130d22aa575cec3
Maksim Sobolev <sobmig@gmail.com>
2022-09-10 19:24:55 +0300

    удалены повторяющиеся строки кода

--- task5.py
+++ task5.py
@@ -10,21 +10,19 @@ def Fib(number):
 
 user_number = int(input("Введите число: "))
 
+plus_fibo_list = Fib(user_number)
+
+minus_fibo_list = plus_fibo_list.copy()
+minus_fibo_list.reverse()
+minus_fibo_list.append(0)
+
 if user_number % 2 == 0:
-    plus_fibo_list = Fib(user_number)
-    minus_fibo_list = plus_fibo_list.copy()
-    minus_fibo_list.reverse()
     for i in range(0, len(minus_fibo_list), 2):
         minus_fibo_list[i] = -minus_fibo_list[i]
-    minus_fibo_list.append(0)
-    minus_fibo_list.extend(plus_fibo_list)
-    print(f"{user_number} -> {minus_fibo_list}")
 else:
-    plus_fibo_list = Fib(user_number)
-    minus_fibo_list = plus_fibo_list.copy()
-    minus_fibo_list.reverse()
     for i in range(1, len(minus_fibo_list), 2):
         minus_fibo_list[i] = -minus_fibo_list[i]
-    minus_fibo_list.append(0)
-    minus_fibo_list.extend(plus_fibo_list)
-    print(f"{user_number} -> {minus_fibo_list}")
+
+minus_fibo_list.extend(plus_fibo_list)
+print(f"{user_number} -> {minus_fibo_list}")
+
